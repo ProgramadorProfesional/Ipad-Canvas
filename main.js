5403970e98079fe7df938fd7ba0cf6e1897ee7de
MaxiToon <116525245+ProgramadorProfesional@users.noreply.github.com>
2023-05-10 19:36:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+mouseEvent = "Empty";
+var last_position_of_x,last_position_of_y;
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+color = "blue";
+width = 3;
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown(e)
+{
+    color = document.getElementById("Color").value;
+    width = document.getElementById("width").value;
+    mouseEvent = "mouseDown";
+}
+canvas.addEventListener("mousemove", my_mousemove);
+function my_mousemove(e)
+{
+ current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+ current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+
+ if (mouseEvent == "mouseDown") {
+ ctx.beginPath();
+ ctx.strokeStyle = color;
+ ctx.lineWidth = width;
+
+ console.log("La última posición de las coordenadas x, y = ");
+ console.log("x = " + last_position_of_x + "y = " 
+      + last_position_of_y);
+ ctx.moveTo(last_position_of_x, last_position_of_y);
+
+ console.log("Posición actual de las coordenadas x, y = ");
+ console.log("x  = " + current_position_of_mouse_x + "y = " 
+         + current_position_of_mouse_y);
+ ctx.lineTo(current_position_of_mouse_x, 
+         current_position_of_mouse_y);
+ ctx.stroke();
+ }
+ last_position_of_x = current_position_of_mouse_x; 
+ last_position_of_y = current_position_of_mouse_y;
+}
+canvas.addEventListener("mouseup", my_mouseup);
+function my_mouseup(e)
+{
+        mouseEvent = "mouseUp";
+}
+canvas.addEventListener("mouseleave", my_mouseleave);
+function my_mouseleave(e)
+{
+        mouseEvent = "mouseLeave";
+}
+
+
+function borrar() { 
+        ctx.clearRect(0, 0, canvas.width, canvas.height); 
+        }
